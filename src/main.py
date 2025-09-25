05524840c637bb73a32351c322da6462156e2f70
ruslan <gamidov.r@mail.ru>
2025-09-25 18:31:54 +0300

    bugfix: extract classes from main.py to models.py. product_count remember all products

--- src/main.py
+++ src/main.py
@@ -1,36 +1,4 @@
-class Product:
-    """класс, описывающий продукты"""
-
-    name: str
-    description: str
-    price: float
-    quantity: float
-
-    def __init__(self, name: str, description: str, price: float, quantity: float):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-
-class Category:
-    """класс, описывающий категории"""
-
-    name: str
-    description: str
-    products: list
-    category_count: int
-    product_count: int
-    product_count = 0  # len(products)
-    category_count = 0
-
-    def __init__(self, name: str, description: str, products: list):
-        self.name = name
-        self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count = len(products)
-
+from src.models import Product, Category
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
