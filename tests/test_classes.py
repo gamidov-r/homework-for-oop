f12bca7a94e85ea8473bfe3cf59a4168a113ac99
ruslan <gamidov.r@mail.ru>
2025-10-07 19:14:39 +0300

    extend test from 47% to 75%

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.classes import Smartphone, LawnGrass
+from src.classes import Category, Product, Smartphone, LawnGrass
 
 
 def test_typeerror():
@@ -8,14 +8,28 @@ def test_typeerror():
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
     grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
     with pytest.raises(TypeError):
         invalid_sum = smartphone1 + grass1
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+    smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+
+    category_smartphones.add_product(smartphone3)
+
+    with pytest.raises(TypeError):
+        category_smartphones.add_product("Not a product")
 
 
 def test_smartphone():
     smartphone1 = Smartphone(
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
     assert smartphone1.name == "Samsung Galaxy S23 Ultra"
     assert smartphone1.description == "256GB, Серый цвет, 200MP камера"
     assert smartphone1.price == 180000.0
@@ -24,6 +38,15 @@ def test_smartphone():
     assert smartphone1.model == "S23 Ultra"
     assert smartphone1.memory == 256
     assert smartphone1.color == "Серый"
+
+    assert smartphone2.name == "Iphone 15"
+    assert smartphone2.description == "512GB, Gray space"
+    assert smartphone2.price == 210000.0
+    assert smartphone2.quantity == 8
+    assert smartphone2.efficiency == 98.2
+    assert smartphone2.model == "15"
+    assert smartphone2.memory == 512
+    assert smartphone2.color == "Gray space"
 
 
 def test_lawngrasses():
@@ -35,3 +58,28 @@ def test_lawngrasses():
     assert grass1.country == "Россия"
     assert grass1.germination_period == "7 дней"
     assert grass1.color == "Зеленый"
+
+
+def test_new_product() -> None:
+    new_product = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+    assert new_product.description.startswith("256GB")
+    assert new_product.price == 180000.0
+    assert new_product.quantity == 5
+
+
+def test_str():
+    smartphone1 = Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
+    )
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    assert str(category_smartphones) == "Смартфоны, количество продуктов: 102 шт."
